1a809032459c4a07b8e3e350fe8daa4562011751
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-07-26 10:57:09 -0300

    providers

---
+++ src/providers/alert/alert.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { AlertController, ToastController } from 'ionic-angular';
+
+@Injectable()
+export class AlertProvider {
+
+  constructor(
+    private alertCtrl: AlertController,
+    private toatCtrl: ToastController) {
+
+  }
+
+  toast(title: string, position: string): void {
+    let toast = this.toatCtrl.create({ message: title, position: position, duration: 3000 });
+    toast.present();
+  }
+
+  alert(title: string, message: string): void {
+    this.alertCtrl.create({
+      title: title,
+      message: message,
+      buttons: ['Ok'],
+      enableBackdropDismiss: false
+    }).present();
+  }
+
+  confirm(title: string, message: string, callback: any): void {
+    this.alertCtrl.create({
+      title: title,
+      message: message,
+      buttons: [
+        {
+          text: "Não", role: 'Cancel', handler: () => {
+            console.log('Confirm: Say no');
+          }
+        },
+        {
+          text: "Sim", handler: () => {
+            callback();
+          }
+        }
+      ]
+    }).present();
+  }
+
+}
