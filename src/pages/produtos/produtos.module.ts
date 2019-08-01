d2b3edc84d25170f04ec71a5ce8f0cba5fdac71e
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-08-01 12:22:31 -0300

    Criando Pipes

--- src/pages/produtos/produtos.module.ts
+++ src/pages/produtos/produtos.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProdutosPage } from './produtos';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
     ProdutosPage,
   ],
   imports: [
+    PipesModule,
     IonicPageModule.forChild(ProdutosPage),
   ],
 })
---
+++ src/pipes/real/real.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { UtilsHelper } from '../../app/helpers/utilsHelper';
+
+@Pipe({
+  name: 'real',
+})
+export class RealPipe implements PipeTransform {
+
+  transform(value: number, ...args) {
+    
+    return UtilsHelper.number.formatCorrency(value);
+    
+  }
+
+}
